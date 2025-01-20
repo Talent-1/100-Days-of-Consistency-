e9444a37dc5d4ead7a5f7b5625da4ffa2f7c320a
Azikiwe Chibuzo <122829769+Talent-1@users.noreply.github.com>
2025-01-20 08:55:29 +0000

    complete code with usge example for day 1 task

---
+++ Day 1/PasswordValidator.js
@@ -0,0 +1,17 @@
+// Regular expression pattern for password validation
+const password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[+=*%$£!>@~#_-:;/])([^\s])[a-zA-z\d+=*%$£!>@~#_-:;/]{8,}$/;
+
+// Function to validate a given password
+function validatePassword(userPassword) {
+  // Check if the password matches the defined regular expression pattern
+  if (password.test(userPassword)) { 
+    return "Valid Password"; 
+  } else { 
+    // Return an error message if the password does not meet the criteria
+    return "Password must be at least 8 characters long and include at least one uppercase, one lowercase, a digit and a special character"; 
+  }
+}
+
+// Example usage:
+let userPassword = `simplr123`; 
+console.log(validatePassword(userPassword)); // Output: Password must be at least 8 characters long and include at least one uppercase, one lowercase, a digit and a special character
